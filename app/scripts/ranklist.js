573208aeaf520621d127dcc99f40300cb1b43f8f
TaeHyeonKim <33569961+socm9145@users.noreply.github.com>
2024-06-16 22:18:02 +0900

    #5 Feat: add the feature that delete the hunters from the list

--- app/scripts/ranklist.js
+++ app/scripts/ranklist.js
@@ -4,10 +4,18 @@ const navElement = document.querySelector('.nav-pills');
 const tableElement = document.querySelector('.table-responsive');
 
 if (navElement && tableElement) {
-  const li = document.createElement('li');
-  li.className = 'active';
-  li.style.cursor = 'pointer';
+  const registerButton = createRegisterButton();
+  const refreshButton = createRefreshButton();
+  navElement.appendChild(registerButton);
+  navElement.appendChild(refreshButton);
+} else {
+  console.error('Cannot find the elements');
+}
 
+function createRegisterButton() {
+  const li = document.createElement('li');
+  li.className = 'register-button';
+  // add register event
   const a = document.createElement('a');
   a.innerText = '헌터 등록';
   a.addEventListener('click', function (event) {
@@ -16,7 +24,7 @@ if (navElement && tableElement) {
     const userList = Array.from(links).map((link) =>
       link.href.split('/').pop()
     );
-
+    // alert the result
     if (userList.length > 0) {
       ext.storage.local.set({ userList: userList }, () => {
         if (ext.runtime.lastError) {
@@ -29,9 +37,26 @@ if (navElement && tableElement) {
       window.alert('등록할 헌터가 없습니다.');
     }
   });
-
   li.appendChild(a);
-  navElement.appendChild(li);
-} else {
-  console.error('Cannot find the elements');
+  return li;
+}
+
+function createRefreshButton() {
+  const li = document.createElement('li');
+  li.className = 'refresh-button';
+  // add refresh event
+  const a = document.createElement('a');
+  a.innerText = '헌터 초기화';
+  a.addEventListener('click', function (event) {
+    event.preventDefault();
+    ext.storage.local.set({ userList: [] }, () => {
+      if (ext.runtime.lastError) {
+        window.alert('헌터 초기화 실패');
+      } else {
+        window.alert('헌터 초기화 완료');
+      }
+    });
+  });
+  li.appendChild(a);
+  return li;
 }
